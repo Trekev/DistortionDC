ecddc6fd95d6b9c5295da2feeea036d37e85db28
Evan Keeler <Trekev91@gmail.com>
2017-03-10 13:54:14 -0500

    Update db settings

--- distortiondc/settings.py
+++ distortiondc/settings.py
@@ -90,10 +90,11 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-DATABASES['default'] = dj_database_url.config(default='postgres://...')
-DATABASES['default'] = dj_database_url.parse('postgres://...', conn_max_age=600)
+DATABASES['default'] = dj_database_url.config(default='sqlite://db/sqlite3.db')
+
+#DATABASES['default'].update(db_from_env)
+#DATABASES['default'] = dj_database_url.config(default='postgres://...')
+#DATABASES['default'] = dj_database_url.parse('postgres://...', conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators

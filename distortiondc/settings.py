8796858fee4cc46c8d2bdf4dd20924e076723bef
Evan Keeler <Trekev91@gmail.com>
2017-03-10 13:44:45 -0500

    Update db settings

--- distortiondc/settings.py
+++ distortiondc/settings.py
@@ -83,12 +83,9 @@ WSGI_APPLICATION = 'distortiondc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES['default'] = dj_database_url.config(default='postgres://...')
+DATABASES['default'] = dj_database_url.parse('postgres://...', conn_max_age=600)
+
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)

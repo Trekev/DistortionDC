7664560956fa624ab5daba5b322a9a623cc52a9c
Evan Keeler <Trekev91@gmail.com>
2017-12-07 11:52:56 -0500

    Added initial database

--- upcoming/models.py
+++ upcoming/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from ckeditor_uploader.fields import RichTextUploadingField
 # Create your models here.
 class Events(models.Model):
     even_id = models.AutoField(primary_key=True)
@@ -11,3 +11,23 @@ class Events(models.Model):
 
     def __str__(self):
         return self.event_name
+
+class Show(models.Model):
+    date = models.DateTimeField(auto_now=False)
+    band = models.CharField(max_length=100)
+    bandgenre = models.CharField(max_length=50)
+    youtube = models.URLField(blank=True)
+    soundcloud = models.URLField(blank=True)
+    spotify = models.URLField(blank=True)
+    venue = models.ForeignKey('Venue', on_delete=models.CASCADE)
+    minprice = models.PositiveIntegerField()
+    maxprice = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.band + " @ " + self.venue.Name
+
+class Venue(models.Model):
+    Name = models.CharField(max_length=100)
+    description = RichTextUploadingField(config_name='awesome_ckeditor', default='Literally ANYTHING')
+    def __str__(self):
+        return self.Name
--- upcoming/views.py
+++ upcoming/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from . import models
 from django.views import generic
+import datetime
+from django.http import HttpResponse
+import simplejson as json
 # Create your views here.
 
 def Upcoming(request):
@@ -9,20 +12,18 @@ def Upcoming(request):
     get_event_types = models.Events.objects.only('event_type')
 
     # if filters applied then get parameter and filter based on condition else return object
-    if request.GET:  
+    if request.GET:
         event_arr = []
         if request.GET.get('event_type') == "all":
-            all_events = Events.objects.all()
-        else:   
-            all_events = Events.objects.filter(event_type__icontains=request.GET.get('event_type'))
+            all_events = models.Events.objects.all()
+        else:
+            all_events = models.Events.objects.filter(event_type__icontains=request.GET.get('event_type'))
 
         for i in all_events:
             event_sub_arr = {}
             event_sub_arr['title'] = i.event_name
-            start_date = datetime.datetime.strptime(str(i.start_date.date()), "%Y-%m-%dT%H:%M:%S").strftime("%Y-%m-%dT%H:%M:%S")
-            end_date = datetime.datetime.strptime(str(i.end_date.date()), "%Y-%m-%d").strftime("%Y-%m-%d")
+            start_date = datetime.datetime.strptime(str(i.start_date.date()), "%Y-%m-%d").strftime("%Y-%m-%d")
             event_sub_arr['start'] = start_date
-            event_sub_arr['end'] = end_date
             event_arr.append(event_sub_arr)
         return HttpResponse(json.dumps(event_arr))
 

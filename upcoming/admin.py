3541e616adfe983ed44b85c575b3b396a5edf577
Evan Keeler <Trekev91@gmail.com>
2017-12-09 18:29:16 -0500

    Updated Database

--- upcoming/admin.py
+++ upcoming/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from .models import Events
+from .models import Genre
 from .models import Show
 from .models import Venue
 
-admin.site.register(Events)
 admin.site.register(Show)
 admin.site.register(Venue)
+admin.site.register(Genre)
 # Register your models here.

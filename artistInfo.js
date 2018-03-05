11e4d786f8b41b7b762a36833b08618b3d3f45bc
Evan Keeler <Trekev91@gmail.com>
2018-03-05 11:38:54 -0500

    added even more to artistInfo

--- artistInfo.js
+++ artistInfo.js
@@ -1,5 +1,14 @@
 const { Client } = require('pg');
 var request = require('request');
+var Spotify = require('spotify-web-api-js');
+var XMLHttpRequest = require("xmlhttprequest");
+
+
+var spotifyApi = new Spotify();
+spotifyApi.setAccessToken('BQAzlJycT22FydDwnNXDMr17yRhP_DWHZnR1oD2SnUnExCaCa9XB3TBljxc0ODw44q1sqYPM7Y9NRhEbqmKaX7EfZC_F9hvvvgeUiLKq3oY-H--I2MCZaNFV-dSthIh8ukYItID_PndHYA');
+
+
+
 
 const client = new Client({
   connectionString: "postgres://zygjgjaweejdzc:5a069c486f07e7d474c26a4a2c2ab0062e981518ebf10d530e9c3ac93849731b@ec2-23-23-212-121.compute-1.amazonaws.com:5432/denhrngn50051u",
@@ -23,14 +32,26 @@ function getArtist(){
     })
   })
   }
-
-  getArtist()
-    .then(function(v) { // `getArtist` returns a promise
-      for(let row of v){console.log(row)}; // iterate through results
-
+function getID(artist){
+  return new Promise((resolve,reject) => {
+      spotifyApi.searchArtists(row, function(err, data) {
+        if (err) console.error(err);
+        if(data['artists']['total']> 0){
+          resolve(data['artists']['items'][0]['id'])
+        }
+        else {idList.push('')};
+      })
     })
-    .catch(function(v) {
-    });
+    }
+  getArtist()
+    .then(v => { // `getArtist` returns a promise
+
+          }
+
+          getID().then(b =>{console.log(b)}).catch()
+
+      })
+    .catch(function(v) { });
 
 // client.query(query, (err,res) =>{
 //
